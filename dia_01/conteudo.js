e779113b8243d1da9ec1a03bb3629521cc69e859
Rafael Peixoto <peixoto@textecnologia.com.br>
2019-11-23 07:42:38 -0300

    ajusta o gitinogre para desconsiderar o gabarito e inclui os arquivos com o conteudo e exercicio do dia 01

---
+++ dia_01/conteudo.js
@@ -0,0 +1,18 @@
+
+/***** Criando variáveis e entendendo os tipos *****/
+
+// Strings
+
+// Numeros
+
+// Booleanos
+
+/***** Condicionais *****/
+
+/***** Laços *****/
+
+// For
+
+// While
+
+// Foreach
---
+++ dia_01/exercicio.js
@@ -0,0 +1,37 @@
+/**
+ * Utilizando o for
+ * Para números de 0 a 20 (incluindo o zero e o vinte), imprima as mensagens conforme
+ * as seguintes regras:
+ *  Se o número for par, imprima "O número x é par";
+ *  Se o número for divísivel por 3, imprima "O número x é divisível por 3"
+ *  Mas caso o número seja par e divisível por 3, imprima "O número é par e também 
+ * divisível por 3"
+ *
+ */
+
+
+
+
+/**
+ * Utilizando o while
+ * Para números de 15 a 10 (incluindo o quinze e o dez), imprima mensagens conforme 
+ * as seguintes regras:
+ * Se o número for divisível por 5, imprima "OK"
+ * Do contrario, imprima "Não OK"
+ */
+
+
+
+
+/**
+ * Dado as seguintes listas de inteiros:
+ * primeiraLista = [0, 8, 4, 6, 2]
+ * segundaLista = [9, 3, 5, 7, 1]
+ * 
+ * Comparando sempre os números da mesma posição em cada uma das listas,
+ * se o número da primeiraLista for maior que o da segunda some os dois,
+ * se o número da segunda lista for maior, subtraio-os, mas se forem iguais
+ * apenas imprima o indíce deste número.
+ */
+
+
