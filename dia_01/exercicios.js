f224bff79c3f97adffd966b6149437af9efb4624
Felipe Magnon <felipemagnon@gmail.com>
2019-11-30 10:50:07 -0300

    exercicios da aula 1

---
+++ dia_01/exercicios.js
@@ -0,0 +1,54 @@
+//for
+/*var resposta = ''
+
+for(atual=0; atual<=20; atual++){
+    resposta = ''
+    if(atual%2 == 0)
+    {
+        if(atual%3 == 0)
+        {
+           resposta += ' É um número PAR e divisível por 3'       
+        } else {
+            resposta += ' É um número PAR'
+        }               
+    } 
+
+    if ((resposta == '') && (atual%3 == 0))
+    {
+       resposta += ' É um número divisível por 3'       
+    } 
+
+    if (resposta != ''){
+        console.log(atual + resposta)
+    }        
+}*/
+
+// while
+
+/*var regra = 10
+while(regra <= 15){
+    if (regra%5 == 0){
+        console.log(regra + ' Ok')
+    } else {
+        console.log(regra + ' Não Ok')
+    }
+    regra++
+}*/
+
+// Forech
+
+var primeiraLista = [0, 8, 5, 6, 2]
+var segundaLista = [9, 3, 5, 7, 1]
+var soma = primeiraLista + segundaLista
+var subtracao = primeiraLista - segundaLista
+
+console.log(primeiraLista.length)
+console.log(segundaLista.length)
+
+for()
+
+/*if (primeiraLista > segundaLista){
+    console.log('A soma é: ' + soma)
+}else {
+    console.log(subtracao)
+}*/
